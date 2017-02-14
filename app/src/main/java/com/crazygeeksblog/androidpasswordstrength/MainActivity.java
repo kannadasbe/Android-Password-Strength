37af69f719c6fc91bb3f516df2449333b6a0728d
KannadasanG <KannadasanG@sulekha.net>
2017-02-14 17:13:48 +0530

    first commit

---
+++ app/src/main/java/com/crazygeeksblog/androidpasswordstrength/MainActivity.java
@@ -0,0 +1,118 @@
+package com.crazygeeksblog.androidpasswordstrength;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etPassword;
+    private TextView tvPasswordStrength;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        tvPasswordStrength = (TextView) findViewById(R.id.tvPasswordStrength);
+
+        // Add password ontext change listener
+        etPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // Calculate password strength
+                calculateStrength(editable.toString());
+            }
+        });
+    }
+
+    private void calculateStrength(String passwordText) {
+        int upperChars = 0, lowerChars = 0, numbers = 0,
+                specialChars = 0, otherChars = 0, strengthPoints = 0;
+        char c;
+
+        int passwordLength = passwordText.length();
+
+        if (passwordLength ==0)
+        {
+            tvPasswordStrength.setText("Invalid Password");
+            tvPasswordStrength.setBackgroundColor(Color.RED);
+            return;
+        }
+
+        //If password length is <= 5 set strengthPoints=1
+        if (passwordLength <= 5) {
+            strengthPoints =1;
+        }
+        //If password length is >5 and <= 10 set strengthPoints=2
+        else if (passwordLength <= 10) {
+            strengthPoints = 2;
+        }
+        //If password length is >10 set strengthPoints=3
+        else
+            strengthPoints = 3;
+        // Loop through the characters of the password
+        for (int i = 0; i < passwordLength; i++) {
+            c = passwordText.charAt(i);
+            // If password contains lowercase letters
+            // then increase strengthPoints by 1
+            if (c >= 'a' && c <= 'z') {
+                if (lowerChars == 0) strengthPoints++;
+                lowerChars = 1;
+            }
+            // If password contains uppercase letters
+            // then increase strengthPoints by 1
+            else if (c >= 'A' && c <= 'Z') {
+                if (upperChars == 0) strengthPoints++;
+                upperChars = 1;
+            }
+            // If password contains numbers
+            // then increase strengthPoints by 1
+            else if (c >= '0' && c <= '9') {
+                if (numbers == 0) strengthPoints++;
+                numbers = 1;
+            }
+            // If password contains _ or @
+            // then increase strengthPoints by 1
+            else if (c == '_' || c == '@') {
+                if (specialChars == 0) strengthPoints += 1;
+                specialChars = 1;
+            }
+            // If password contains any other special chars
+            // then increase strengthPoints by 1
+            else {
+                if (otherChars == 0) strengthPoints += 2;
+                otherChars = 1;
+            }
+        }
+
+        if (strengthPoints <= 3)
+        {
+            tvPasswordStrength.setText("Password Strength : LOW");
+            tvPasswordStrength.setBackgroundColor(Color.RED);
+        }
+        else if (strengthPoints <= 6) {
+            tvPasswordStrength.setText("Password Strength : MEDIUM");
+            tvPasswordStrength.setBackgroundColor(Color.YELLOW);
+        }
+        else if (strengthPoints <= 9){
+            tvPasswordStrength.setText("Password Strength : HIGH");
+            tvPasswordStrength.setBackgroundColor(Color.GREEN);
+        }
+    }
+}
+
